4d6d7c8c2cab0eb3bfec7e53b01cfd1f993c3c14
AmenoAmena <ardacavusoglu666@gmail.com>
2024-07-07 15:00:21 +0300

    Prices shown in html

--- Content/prices.py
+++ Content/prices.py
@@ -1,7 +1,29 @@
 import yfinance as yf
 
-ticker_symbol = "AAPL"
-stock = yf.Ticker(ticker_symbol)
-current_price = stock.history(period="1d")['Close'][-1]
-rounded_price = round(current_price)
-print(f"The current price of {ticker_symbol} is: ${rounded_price}")
+
+#Apple Inc. (AAPL)
+#Microsoft Corporation (MSFT)
+#Amazon.com Inc. (AMZN)
+#Alphabet Inc. (GOOGL)
+#Tesla, Inc. (TSLA)
+#Meta Platforms, Inc. (META)
+#NVIDIA Corporation (NVDA)
+#Coca-Cola Company (KO)
+#Intel Corporation (INTC)
+#Visa Inc. (V)
+
+
+def getting_prices():
+    prices = {}
+    ticker_symbols = ["AAPL", "MSFT", "AMZN", "GOOGL", "TSLA", "META", "NVDA", "KO", "INTC", "V"]
+    
+    for ticker in ticker_symbols:
+        stock = yf.Ticker(ticker)
+        current_price = stock.history(period="1d")['Close'].iloc[-1]
+        rounded_price = round(current_price, 2)
+        company_name = stock.info['longName']  
+        
+        prices[company_name] = rounded_price
+    
+    return prices
+
--- Content/views.py
+++ Content/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .prices import getting_prices
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
-    
+    prices = getting_prices()
+    return render(request, 'Content/index.html',{
+        'prices_dict': prices.items()
+    })
