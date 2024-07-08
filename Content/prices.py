69fdad9128b52a5ff0abd00011f9ae4246223f59
AmenoAmena <ardacavusoglu666@gmail.com>
2024-07-08 05:19:23 +0300

    Make prices class and each company diffrent share get_price for optimasation ı am not sure it will be faster just trying

--- Content/prices.py
+++ Content/prices.py
@@ -13,19 +13,55 @@ import yfinance as yf
 #Visa Inc. (V)
 
 
-def getting_prices():
-    prices = {}
-    ticker_symbols = ["AAPL", "MSFT", "AMZN", "GOOGL", "TSLA", "META", "NVDA", "KO", "INTC", "V"]
+#def getting_prices():
+#    prices = {}
+#    ticker_symbols = ["AAPL", "MSFT", "AMZN", "GOOGL", "TSLA", "META", "NVDA", "KO", "INTC", "V"]
+#    
+#    for ticker in ticker_symbols:
+#        stock = yf.Ticker(ticker)
+#        current_price = stock.history(period="1d")['Close'].iloc[-1]
+#        rounded_price = round(current_price, 2)
+#        company_name = stock.info['longName']  
+#        
+#        prices[company_name] = rounded_price
+#    
+#    return prices
+
+class Price:
+    def __init__(self):
+        pass
     
-    for ticker in ticker_symbols:
+    def get_price(self, ticker):
         stock = yf.Ticker(ticker)
         current_price = stock.history(period="1d")['Close'].iloc[-1]
-        rounded_price = round(current_price, 2)
-        company_name = stock.info['longName']  
-        
-        prices[company_name] = rounded_price
+        return round(current_price, 2)
     
-    return prices
+    def google_price(self):
+        return self.get_price("GOOGL")
+    
+    def apple_price(self):
+        return self.get_price("AAPL")
+    
+    def microsoft_price(self):
+        return self.get_price("MSFT")
+    
+    def amazon_price(self):
+        return self.get_price("AMZN")
+    
+    def tesla_price(self):
+        return self.get_price("TSLA")
+            
+    def Nvidia_price(self):
+        return self.get_price("NVDA")
 
-print(getting_prices())
+    def Visa_price(self):
+        return self.get_price("TSLA")
 
+    def Coca-Cola_price(self):
+        return self.get_price("KO")
+
+    def Intel_price(self):
+        return self.get_price("INTC")
+        
+    def Meta_price(self):
+        return self.get_price("META")
