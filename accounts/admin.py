b83ae102a98deda6d7c9863d6c92a928292fe163
AmenoAmena <ardacavusoglu666@gmail.com>
2024-07-08 07:38:35 +0300

    Something happened weird

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Stock,User
+from .models import Stock,UserStock
 
 # Register your models here.
 admin.site.register(Stock)
-admin.site.register(User)
+admin.site.register(UserStock)
