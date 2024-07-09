a2d1125ff08a921cce0616cb61381e6856b63fe3
AmenoAmena <ardacavusoglu666@gmail.com>
2024-07-09 18:21:21 +0300

    Trying to show stock prices

--- Content/views.py
+++ Content/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from accounts.models import stock_user
+from accounts.models import stock_user,UserStock
 from .prices import Price
 from django.contrib.auth.decorators import login_required
 
@@ -13,9 +13,13 @@ def index(request):
     stock_value = user_instance.get_total_stock_value()
     user_cash = user.money
     made_money = user_money - 10000
+    user_stocks = UserStock.objects.filter(user=user)
+    print(user_stocks)
     return render(request, 'Content/index.html', {
         'user_money': user_money,
         'stock_value':stock_value,
         'user_cash':user_cash,
         'made_money':made_money,
+        'user_stocks':user_stocks
     })
+
