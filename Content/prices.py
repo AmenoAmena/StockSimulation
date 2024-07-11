cd105ce0ebe1ff7ad9d60afbff88a8c90af0a023
AmenoAmena <ardacavusoglu666@gmail.com>
2024-07-12 02:39:27 +0300

    Changed the price fetching to automate every stock

--- Content/prices.py
+++ Content/prices.py
@@ -13,58 +13,7 @@ import yfinance as yf
 #Visa Inc. (V)
 
 
-def getting_prices():
-    prices = {}
-    ticker_symbols = ["AAPL", "MSFT", "AMZN", "GOOGL", "TSLA", "META", "NVDA", "KO", "INTC", "V"]
-    
-    for ticker in ticker_symbols:
-        stock = yf.Ticker(ticker)
-        current_price = stock.history(period="1d")['Close'].iloc[-1]
-        rounded_price = round(current_price, 2)
-        company_name = stock.info['longName']  
-        
-        prices[company_name] = rounded_price
-    
-    return prices
-
-class Price:
-    def __init__(self):
-        pass
-    
-    def get_price(self, ticker):
-        stock = yf.Ticker(ticker)
-        current_price = stock.history(period="1d")['Close'].iloc[-1]
-        return round(current_price, 2)
-    
-    def google_price(self):
-        return self.get_price("GOOGL")
-    
-    def apple_price(self):
-        return self.get_price("AAPL")
-    
-    def microsoft_price(self):
-        return self.get_price("MSFT")
-    
-    def amazon_price(self):
-        return self.get_price("AMZN")
-    
-    def tesla_price(self):
-        return self.get_price("TSLA")
-    
-    def nvidia_price(self):
-        return self.get_price("NVDA")
-
-    def visa_price(self):
-        return self.get_price("V")
-
-    def coca_cola_price(self):
-        return self.get_price("KO")
-
-    def intel_price(self):
-        return self.get_price("INTC")
-    
-    def meta_price(self):
-        return self.get_price("META")
-
-tracker = Price()
-print(tracker.nvidia_price())
+def get_price(ticker):
+    stock = yf.Ticker(ticker)
+    current_price = stock.history(period="1d")['Close'].iloc[-1]
+    return round(current_price, 2)
